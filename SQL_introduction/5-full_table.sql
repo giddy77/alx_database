404c2db0358a71d1b6cdd1342ccd8a36df51549f
Gideon <giddysipoi@gmail.com>
2023-08-17 13:38:20 +0300

    updates

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- describing the table first_table without using desc
-mysql -u your_username -p -e "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+mysql -u your_username -p -e "SELECT CONCAT('CREATE TABLE `first_table` (', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, ' ', IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL')), ',', 'PRIMARY KEY (`', GROUP_CONCAT(COLUMN_NAME), '`) ) ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME, ';')
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';"
