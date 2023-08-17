6a80685a22e2d1497d590bde869aef0a0d0503af
Gideon <giddysipoi@gmail.com>
2023-08-17 13:32:15 +0300

    create a table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
 --creating a table in the current mysql server
 
-CREATE TABLE first_table (
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
