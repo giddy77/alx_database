ab0a5b434c98a2169f8cb9722e9c5e2feb57d990
Gideon <giddysipoi@gmail.com>
2023-08-17 13:50:46 +0300

    insert  a new record in the db

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- insert a new record into the table
-mysql -u your_username -p -e "INSERT INTO first_table(id,name) values(89,'Holberton School');"
+mysql -u your_username -p -e "INSERT INTO first_table(id,name) values(89,'Holberton School'); SELECT * FROM hbtn_0c_0.first_table WHERE id = 89;"
