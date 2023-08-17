a1c5820a0ff6e751c4fe729c12c68113bbfdf406
Gideon <giddysipoi@gmail.com>
2023-08-17 13:54:57 +0300

    rows in the table

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
 -- shows the number of rows with id 89
-mysql -u your_username -p -e "SELECT COUNT(*) FROM first_table WHERE id = 89;
-"
+mysql -u your_username -p -e "SELECT COUNT(*) FROM first_table WHERE id = 89;"
