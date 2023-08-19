22ab3ed9c0a900ac894f9f1b7943f3bdcd30d65e
Gideon <giddysipoi@gmail.com>
2023-08-20 01:15:50 +0300

    updates

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 --this is selecting everything from the first_table 
-SELECT *
+SELECT name ,created_at
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
