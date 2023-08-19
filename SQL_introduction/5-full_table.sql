90d3eece253fcfb83b1b88184012de90c83eb6e5
Gideon <giddysipoi@gmail.com>
2023-08-20 01:29:04 +0300

    updates

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,14 @@
---this is selecting everything from the first_table 
-mysql -u username -p hbtn_0c_0;
-SELECT * FROM first_table;
+--here is the query
+SELECT CONCAT(
+    'CREATE TABLE `', TABLE_NAME, '`(',
+    GROUP_CONCAT(
+        CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE, ' ',
+               IF(IS_NULLABLE = 'NO', 'NOT NULL', 'NULL'),
+               IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
+               IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')), 
+        SEPARATOR ',\n'
+    ),
+    ');'
+)
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
