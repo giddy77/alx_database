87a958f17653fc5091f0c3dd8e68ed7705b4e021
Gideon <giddysipoi@gmail.com>
2023-08-20 01:35:05 +0300

    updates

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,4 @@
---here is the query
-SELECT CONCAT(
-    'CREATE TABLE `', TABLE_NAME, '`(',
-    GROUP_CONCAT(
-        CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE, ' ',
-               IF(IS_NULLABLE = 'NO', 'NOT NULL', 'NULL'),
-               IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
-               IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')), 
-        SEPARATOR ',\n'
-    ),
-    ');'
-)
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+--THIS IS CODE
+use hbtn_0c_0;
+
+SELECT *.first_table;
