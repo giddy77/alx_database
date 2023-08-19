933861b0624ae2c21780733b11b3fae5b9fef697
Gideon <giddysipoi@gmail.com>
2023-08-19 20:23:55 +0300

    updates

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,9 @@
 --creating a table in the current mysql server
 
 -- Create the table if it doesn't exist
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256);
 );
+hbtn_0c_0
