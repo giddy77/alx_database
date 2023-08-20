db9630efe46659e397c69987bf36d665323cae46
Gideon <giddysipoi@gmail.com>
2023-08-20 16:00:37 +0300

    updates

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- creates a table force_name with the following description
-CREATE TABLE force_name IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS force_name(
     id INT,
     name VARCHAR(256) NOT NULL
 );
