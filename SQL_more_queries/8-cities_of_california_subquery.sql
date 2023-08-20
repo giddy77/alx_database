369c82e678c237cd65d66016fd69dd3e0b780d2f
Gideon <giddysipoi@gmail.com>
2023-08-20 16:29:50 +0300

    updates

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 
 USE hbtn_0d_usa;
 
-SELECT * FROM cities WHERE state_id (SELECT id from states WHERE states.name = 'California');
+SELECT * FROM cities WHERE state_id (SELECT id FROM states WHERE states.name = 'California');
