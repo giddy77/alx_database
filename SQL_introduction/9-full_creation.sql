5569c4f0d72a0099e720af4f33f96b056280ccbf
Gideon <giddysipoi@gmail.com>
2023-08-20 03:34:56 +0300

    updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -13,4 +13,14 @@ INSERT INTO second_table (name, score) VALUES
     ('George', 8);
 
 
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
 
+INSERT INTO second_table (name, score) VALUES
+    ('A', 12),
+    ('B', 12),
+    ('C', 12),
+    ('D', 12);
