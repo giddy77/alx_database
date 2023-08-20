4a06d0aef6d33e790785a023529423c0e87951f4
Gideon <giddysipoi@gmail.com>
2023-08-20 03:41:14 +0300

    updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,20 +1,20 @@
 -- create a second table and inserting records
 
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256),
     score INT
 );
 
-INSERT INTO second_table (id,name, score) VALUES
-    ('John', 10),
-    ('Alex', 3),
-    ('Bob', 14),
-    ('George', 8);
+INSERT INTO second_table (name, score) VALUES
+    (1,'John', 10),
+    (2,'Alex', 3),
+    (3,'Bob', 14),
+    (4,'George', 8);
 
 
 INSERT INTO second_table IF EXISTS (name, score) VALUES
-    ('A', 12),
-    ('B', 12),
-    ('C', 12),
-    ('D', 12);
+    (5,'A', 12),
+    (6,'B', 12),
+    (7,'C', 12),
+    (8,'D', 12);
