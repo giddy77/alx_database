e4074e12a73d6ee9887fa306c3f5cac318bbb907
Gideon <giddysipoi@gmail.com>
2023-08-20 03:05:27 +0300

    updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- create a second table and inserting records
 
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256),
     score INT
 );
