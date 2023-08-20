b520598018260175cf826c24a81fc738e6c58b3a
Gideon <giddysipoi@gmail.com>
2023-08-20 03:31:23 +0300

    updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -12,11 +12,5 @@ INSERT INTO second_table (name, score) VALUES
     ('Bob', 14),
     ('George', 8);
 
-INSERT INTO second_table (name, score) 
-SELECT 'A', 12 UNION ALL
-SELECT 'B', 12 UNION ALL
-SELECT 'C', 12 UNION ALL
-SELECT 'D', 12
-WHERE EXISTS (SELECT 1 FROM second_table);
 
 
